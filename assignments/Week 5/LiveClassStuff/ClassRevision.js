6cafd20399ed51e58421c7cf855331413102c1c1
Gaurav Yadav <gyadav187@gmail.com>
2025-02-11 19:01:14 +0530

    This is class stuff

---
+++ assignments/Week 5/LiveClassStuff/ClassRevision.js
@@ -0,0 +1,7 @@
+//Problem: Create an object representing a type of tea with properties for name, type, and caffeine content.
+
+const lemonTea = {
+    name: "Lemon Tea",
+    type : "What do I know",
+    caffeine : "Less"
+}
