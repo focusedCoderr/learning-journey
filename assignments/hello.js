ca7148e8760c21bb3c01fc71e5037eb27214de48
Gaurav Yadav <gyadav187@gmail.com>
2025-02-11 18:18:29 +0530

    This is my first commit

---
+++ assignments/hello.js
@@ -0,0 +1,6 @@
+console.log("This is just a dummy file to know how git works");
+console.log("I have given git init command in learning folder");
+console.log("Then I added a folder assignments and did git add .");
+console.log("But nothing happened, because i think git tracks files");
+console.log("so inside assignment folder, I have added this file. Lets see what happens now. When I push the learning folder, I think the assignement folder will also go to github");
+
