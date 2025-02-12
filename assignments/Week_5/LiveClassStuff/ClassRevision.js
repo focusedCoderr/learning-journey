84de88e8073fa7a730c69548a045c31db592d9a3
Gaurav Yadav <gyadav187@gmail.com>
2025-02-12 12:48:01 +0530

    Two problems solved. trying to commit and push now

--- assignments/Week_5/LiveClassStuff/ClassRevision.js
+++ assignments/Week_5/LiveClassStuff/ClassRevision.js
@@ -1,7 +1,13 @@
 //Problem: Create an object representing a type of tea with properties for name, type, and caffeine content.
 
-const lemonTea = {
+const teas = {
     name: "Lemon Tea",
     type : "What do I know",
     caffeine : "Less"
 }
+
+//Access and print the name and type properties of the tea object
+
+console.log(`The name of the tea is ${teas.name} and the type is ${teas.type} `);
+
+// Add 
