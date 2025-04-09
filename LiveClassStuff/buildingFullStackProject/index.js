d063ae74e908c0d4dfff4fe0712d9b7382966828
Gaurav Yadav Ubuntu <gyadav187@gmail.com>
2025-04-09 12:46:32 +0530

    Installed Express .env ignored

--- LiveClassStuff/buildingFullStackProject/index.js
+++ LiveClassStuff/buildingFullStackProject/index.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Cohort");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${PORT}`);
+});
