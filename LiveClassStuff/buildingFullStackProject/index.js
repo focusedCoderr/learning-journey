468f2a1137c7a0ab0c04fea896e8f9e37cc73467
Gaurav Yadav <gyadav187@gmail.com>
2025-04-09 16:29:13 +0530

    Learning about cors

--- LiveClassStuff/buildingFullStackProject/index.js
+++ LiveClassStuff/buildingFullStackProject/index.js
@@ -1,14 +1,25 @@
 import express from "express";
-import "dotenv/config";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
+
+app.use(cors({}));
 
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-  res.send("Cohort");
+	res.send("Cohort");
+});
+
+app.get("/gaurav", (req, res) => {
+	res.json({
+		name: "Gaurav",
+	});
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening at ${PORT}`);
+	console.log(`Server is listening at ${PORT}`);
 });
---
+++ LiveClassStuff/buildingFullStackProject/notes.md
@@ -0,0 +1,4 @@
+####Errors
+
+error : routes always start with '/'. if you do app.get("gaurav" , ()=>{}) you
+will get an error. instead do app.get("/gaurav", ()=>{})
