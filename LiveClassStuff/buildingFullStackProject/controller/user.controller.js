3cf9ea265e7006f904c621ab253d302a4c5e8cdc
Gaurav Yadav Ubuntu <gyadav187@gmail.com>
2025-04-10 15:13:58 +0530

    -A

--- LiveClassStuff/buildingFullStackProject/controller/user.controller.js
+++ LiveClassStuff/buildingFullStackProject/controller/user.controller.js
@@ -1,5 +1,13 @@
 const registerUser = async (req, res) => {
-	res.send("Registered");
+  // res.send("Registered");
+  const { email, name, password } = req.body;
+  console.log(email, name, password);
 };
 
-export { registerUser };
+const acceptPost = async (req, res) => {
+  console.log("i am accepting post request");
+  const { email, name, password } = req.body;
+  console.log(email, name, password);
+};
+
+export { registerUser, acceptPost };
