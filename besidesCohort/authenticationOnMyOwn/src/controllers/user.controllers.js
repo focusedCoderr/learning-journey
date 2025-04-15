f824e37c44c3dece4fc109f217f5511b1e29424c
Gaurav Yadav <gyadav187@gmail.com>
2025-04-15 16:01:26 +0530

    Writing registerUser

--- besidesCohort/authenticationOnMyOwn/src/controllers/user.controllers.js
+++ besidesCohort/authenticationOnMyOwn/src/controllers/user.controllers.js
@@ -1,8 +1,27 @@
+import { User } from "../models/user.models.js";
+
 const checkWorkingFunctionality = async (req, res) => {
 	res.status(201).json({
 		message: "basic working properly",
 	});
 };
 
-const registerUser = (req, res) => {};
+const registerUser = async (req, res) => {
+	const { name, email, password } = req.body;
+
+	if (!name || !email || !password) {
+		return res.status(400).json({
+			message: "Please enter all credentials",
+		});
+	}
+
+	const existingUser = await User.findOne({ email });
+	if (existingUser) {
+		return res.status(400).json({
+			message: "User is already registered",
+		});
+	}
+
+	User.create();
+};
 export { checkWorkingFunctionality, registerUser };
